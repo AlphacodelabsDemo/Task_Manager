25821df36fa3de1bb7271d6ace54bda1ba205684
Anandhu Ashok K <119148713+Ashokanandhu@users.noreply.github.com>
2023-06-07 13:49:21 +0530

    Update and rename server.js.js to server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
 // Import required modules
 const express = require("express");
-<<<<<<< HEAD:server/server.js.js
 const AuthRoutes = require("./routes/authRoutes");
 const taskRoutes = require('./routes/taskRoutes')
 
 //express app
-=======
 const cors = require("cors");
->>>>>>> 2871bafecd7aaaaef3f64866940960c3d3767ce1:server/app.js
 const app = express();
 
 // Load environment variables
 require("dotenv").config();
 
-<<<<<<< HEAD:server/server.js.js
-const cors = require("cors");
 // middlewares
 app.use((req,res,next) =>{
   console.log(req.path,req.method)
@@ -34,14 +29,9 @@ const dbConnection = require("./config/database");
 
 
 // middlewares
-=======
 // Import database connection function
 const dbConnection = require("./config/database");
 
-// Middlewares
->>>>>>> 2871bafecd7aaaaef3f64866940960c3d3767ce1:server/app.js
-app.use(express.json());
-app.use(cors());
 
 // Import routes
 const authRoutes = require("./routes/authRoutes");
@@ -50,21 +40,14 @@ const authRoutes = require("./routes/authRoutes");
 app.use('/api/auth', authRoutes);
 
 // Get the port from environment variables
-<<<<<<< HEAD:server/server.js.js
-const PORT = process.env.PORT;
-
-=======
 const PORT = process.env.PORT || 8080; // Provide a default port if not specified in the environment
->>>>>>> 2871bafecd7aaaaef3f64866940960c3d3767ce1:server/app.js
+
 
 // Function to start the server
 const startServer = async () => {
   // Connect to the database
   await dbConnection();
-<<<<<<< HEAD:server/server.js.js
-=======
 
->>>>>>> 2871bafecd7aaaaef3f64866940960c3d3767ce1:server/app.js
   // Start listening on the specified port
   app.listen(PORT, () => {
     console.log("Server is listening on PORT: " + PORT);
