dfb7c953c8958571dd44f31d3699de804da0346b
Anandhu Ashok K <119148713+Ashokanandhu@users.noreply.github.com>
2023-06-07 14:14:08 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/UserController');
 const loginController = require('../controllers/LoginController');
 
 // Define routes
-router.post('/login', loginController.loginUser); // Use single quotes for string literals
+router.post('/signin', loginController.loginUser); // Use single quotes for string literals
 router.post('/signup', userController.createUser); // Use camelCase for variable and function names
 
 module.exports = router;
